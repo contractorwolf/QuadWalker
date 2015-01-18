2af49e936be61a266340650d3d6a5d53536e6856
contractorwolf <contractorwolf@hotmail.com>
2015-01-18 02:27:40 -0500

    software serial, better power, fixed legs

--- QuadWalker.ino
+++ QuadWalker.ino
@@ -1,4 +1,9 @@
 
+#include <SoftwareSerial.h>
+
+
+
+
 #define TORSOCHANNEL 0
 #define LEGCHANNEL 2
 
@@ -19,8 +24,8 @@
 
 
 int servoCenter = 700;
-int torsoMaxMovement = 70;//220 max
-int legMaxMovement = 130;//140 max
+int torsoMaxMovement = 45;//220 max
+int legMaxMovement = 75;//140 max
 
 boolean isTorsoActive = true;
 boolean isLegActive = true;
@@ -30,64 +35,86 @@ int legMovementDelay = 600;//160;//280 max
 
 int rearOffset = 70;
 
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+
+
 
 void setup()  {
   //Servo Control board
-  Serial1.begin(2400);
+  //Serial1.begin(2400);
+  
+  mySerial.begin(2400);
+  
+  
 
   Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
   Serial.println("QUAD WALKER started");
   
   
-  MoveServo(TORSOCHANNEL, 0);
-  MoveServo(LEGCHANNEL, 0);
+  AltMoveServo(TORSOCHANNEL, 0);
+  AltMoveServo(LEGCHANNEL, 0);
   
-  MoveServo(TORSOCHANNEL2, 0);
-  MoveServo(LEGCHANNEL2, 0);
+  AltMoveServo(TORSOCHANNEL2, 0);
+  AltMoveServo(LEGCHANNEL2, 0);
   
  
-  MoveServo(TORSOCHANNEL3, 0);
-  MoveServo(LEGCHANNEL3, 0);
+  AltMoveServo(TORSOCHANNEL3, 0);
+  AltMoveServo(LEGCHANNEL3, 0);
   
   
-  MoveServo(TORSOCHANNEL4, 0);
-  MoveServo(LEGCHANNEL4, 0);
+  AltMoveServo(TORSOCHANNEL4, 0);
+  AltMoveServo(LEGCHANNEL4, 0);
   
   delay(5000);
 }
 
-
+bool direction = false;
 void loop() {
+  
+
+  
+  
+  
 
   if(isTorsoActive){
-      MoveServo(TORSOCHANNEL, -torsoMaxMovement);//-220
-      MoveServo(TORSOCHANNEL2, torsoMaxMovement);//220   
-      MoveServo(TORSOCHANNEL3, -torsoMaxMovement);//-220
-      MoveServo(TORSOCHANNEL4, torsoMaxMovement);//-220
+      AltMoveServo(TORSOCHANNEL, -torsoMaxMovement);//-220
+      AltMoveServo(TORSOCHANNEL2, torsoMaxMovement);//220   
+      
+      AltMoveServo(TORSOCHANNEL3, -torsoMaxMovement);//-220
+      AltMoveServo(TORSOCHANNEL4, torsoMaxMovement);//-220
       
       delay(torsoMovementDelay);
   }    
       
   if(isLegActive){      
-      MoveServo(LEGCHANNEL, legMaxMovement);//140
-      MoveServo(LEGCHANNEL2, -legMaxMovement);//-140
-      MoveServo(LEGCHANNEL3, -(legMaxMovement));//140    
-      MoveServo(LEGCHANNEL4, legMaxMovement);//140      
+      AltMoveServo(LEGCHANNEL, legMaxMovement);//140
+      AltMoveServo(LEGCHANNEL2, -legMaxMovement);//-140
+      
+      AltMoveServo(LEGCHANNEL3, -legMaxMovement);//140    
+      AltMoveServo(LEGCHANNEL4, legMaxMovement);//140      
       delay(legMovementDelay);
   }
   if(isTorsoActive){   
-      MoveServo(TORSOCHANNEL, torsoMaxMovement);//220
-      MoveServo(TORSOCHANNEL2, -torsoMaxMovement);//-220
-      MoveServo(TORSOCHANNEL3, torsoMaxMovement);//220
-      MoveServo(TORSOCHANNEL4, -torsoMaxMovement);//-220      
+      AltMoveServo(TORSOCHANNEL, torsoMaxMovement);//220
+      AltMoveServo(TORSOCHANNEL2, -torsoMaxMovement);//-220
+      
+      AltMoveServo(TORSOCHANNEL3, torsoMaxMovement);//220
+      AltMoveServo(TORSOCHANNEL4, -torsoMaxMovement);//-220      
       delay(torsoMovementDelay);
   }
   
   if(isLegActive){      
-      MoveServo(LEGCHANNEL, -legMaxMovement);//-140
-      MoveServo(LEGCHANNEL2, legMaxMovement);//140
-      MoveServo(LEGCHANNEL3, legMaxMovement);//-140      
-      MoveServo(LEGCHANNEL4, -legMaxMovement);//-140     
+      AltMoveServo(LEGCHANNEL, -legMaxMovement);//-140
+      AltMoveServo(LEGCHANNEL2, legMaxMovement);//140
+      
+      AltMoveServo(LEGCHANNEL3, legMaxMovement);//-140      
+      AltMoveServo(LEGCHANNEL4, -legMaxMovement);//-140     
       delay(legMovementDelay);
   }    
 
@@ -117,10 +144,38 @@ void MoveServo(int servo_channel, int movement){
     
     
     Serial.print("channel:");
-    Serial.println(servo_channel);
+    Serial.print(servo_channel);
        
-    Serial.print("angle:");
+    Serial.print(" angle:");
     Serial.println(servo_angle);
 }
 
+
+void AltMoveServo(int servo_channel, int movement){
+
+  
+    int servo_angle = servoCenter + movement;
+  
+  
+    byte lsb = servo_angle;
+    byte msb =  servo_angle >> 8;
+    int ramp = 0;
+  
+  
+    mySerial.write(33);  //!
+    mySerial.write(83);  //S
+    mySerial.write(67);  //C
+    mySerial.write(servo_channel);  //channel
+    mySerial.write(ramp);  //rampspeed
+    mySerial.write(lsb);  //LSB
+    mySerial.write(msb);  //MSB
+    mySerial.write(13);
+    
+    
+    Serial.print("channel:");
+    Serial.print(servo_channel);
+       
+    Serial.print(" angle:");
+    Serial.println(servo_angle);
+}
 
